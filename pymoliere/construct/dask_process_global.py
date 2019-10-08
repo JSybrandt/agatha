b75b7ec9938033ee2fdb453db82f4d997e24f65f
JSybrandt <nalta45@gmail.com>
2019-10-08 16:35:34 -0400

    Changes DPG so that restarted nodes should re-collect the shared state.

--- pymoliere/construct/dask_process_global.py
+++ pymoliere/construct/dask_process_global.py
@@ -7,6 +7,8 @@ startup, and we don't want to reload each function call.
 
 from typing import Callable, Any
 from dask.distributed import Lock, Worker, Client, get_worker
+import cloudpickle
+from pathlib import Path
 # An initialize is a function that does not take arguments, and produces an
 # expensive-to-load piece of data.
 
@@ -16,6 +18,7 @@ _PROCESS_GLOBAL_DATA = {}
 
 _INITIALIZERS = {}
 
+PRELOADER_PATH = Path("~/.pymoliere_global_preloader").expanduser()
 
 class WorkerPreloader(object):
   def __init__(self):
@@ -48,9 +51,23 @@ class WorkerPreloader(object):
 
 
 def add_global_preloader(client:Client, preloader:WorkerPreloader)->None:
+  with open(PRELOADER_PATH, 'wb') as f:
+    cloudpickle.dump(preloader, f)
   client.register_worker_plugin(preloader, name="global_preloader")
+
+
+def get_global_preloader():
+  worker = get_worker()
+  if "global_preloader" not in worker.plugins:
+    # This is run if the worker disconnects and reconnects.
+    with open(PRELOADER_PATH, 'rb') as f:
+      worker.plugins["global_preloader"] = cloudpickle.load(f)
+      worker.plugins["global_preloader"].setup(worker)
+  return worker.plugins["global_preloader"]
+
 
 def get(key:str)->Any:
   "Gets a value from the global preloader"
   worker = get_worker()
-  return worker.plugins["global_preloader"].get(key, worker)
+  return get_global_preloader().get(key, worker)
+

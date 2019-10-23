0fc641d4579bc21679dce5b43fd50ec8d12e6a41
JSybrandt <nalta45@gmail.com>
2019-10-23 13:37:35 -0400

    Introduces helper functions to mask sentence pairs
    Adds the ability to generate and apply a mask. Adds unit tests for both
    as well.

--- pymoliere/ml/abstract_generator/test_abstract_generator.py
+++ pymoliere/ml/abstract_generator/test_abstract_generator.py
@@ -3,6 +3,7 @@ from transformers import BertTokenizer, AdamW
 import torch
 from torch.nn.utils.rnn import pad_sequence
 import numpy as np
+from copy import copy
 
 SENTENCE_1 = (
     "Hypothesis generation is becoming a crucial time-saving technique which "
@@ -102,3 +103,167 @@ def test_bert_generator_backwards():
   print(init_hidden_values.shape)
   # We want to make sure that the weights have changed
   assert not np.allclose(init_hidden_values, final_hidden_values)
+
+
+def test_mask_sentence():
+  tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+  original_sequence = tokenizer.encode(
+      text=SENTENCE_1,
+      text_pair=SENTENCE_2,
+      add_special_tokens=True
+  )
+  mask = [False] * len(original_sequence)
+  mask[2] = mask[-2] = True
+  # do the expected manually
+  expected = copy(original_sequence)
+  expected[-2] = expected[2] = tokenizer.mask_token_id
+  expected[2] = tokenizer.mask_token_id
+  # get actual
+  actual = util.mask_sequence(
+      tokenizer=tokenizer,
+      sequence=original_sequence,
+      mask=mask
+  )
+  # require that actual is a modified copy
+  assert original_sequence != actual
+  assert actual == expected
+
+
+def generate_sentence_mask():
+  tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+  first_sent_seq = tokenizer.encode(SENTENCE_1, add_special_tokens=True)
+  full_sequence = tokenizer.encode(
+      text=SENTENCE_1,
+      text_pair=SENTENCE_2,
+      add_special_tokens=True
+  )
+  valid_mask_values = set(range(len(first_sent_seq), len(full_sequence)))
+  # MASK EVERYTHING
+  mask = util.generate_sentence_mask(tokenizer, full_sequence, 1)
+  assert len(mask) == len(full_sequence)
+  for idx, is_masked in enumerate(mask):
+    if is_masked:
+      assert idx in valid_mask_values
+
+
+def test_group_sentences_into_pairs_sorted():
+  records = [{
+        "sent_text": "title",
+        "sent_idx": 0,
+        "sent_total": 3,
+        "pmid": 123,
+        "version": 1,
+      }, {
+        "sent_text": "first",
+        "sent_idx": 1,
+        "sent_total": 3,
+        "pmid": 123,
+        "version": 1,
+      }, {
+        "sent_text": "second",
+        "sent_idx": 2,
+        "sent_total": 3,
+        "pmid": 123,
+        "version": 1,
+  }]
+  actual = util.group_sentences_into_pairs(records)
+  expected = [
+      ("title", "first"),
+      ("first", "second"),
+  ]
+  assert set(actual) == set(expected)
+
+def test_group_sentences_into_pairs_unsorted():
+  # This sort order is difficult because when we see "first" we have to
+  # generate both pairs at that time
+  records = [{
+        "sent_text": "second",
+        "sent_idx": 2,
+        "sent_total": 3,
+        "pmid": 123,
+        "version": 1,
+      }, {
+        "sent_text": "title",
+        "sent_idx": 0,
+        "sent_total": 3,
+        "pmid": 123,
+        "version": 1,
+      }, {
+        "sent_text": "first",
+        "sent_idx": 1,
+        "sent_total": 3,
+        "pmid": 123,
+        "version": 1,
+  }]
+  actual = util.group_sentences_into_pairs(records)
+  expected = [
+      ("title", "first"),
+      ("first", "second"),
+  ]
+  assert set(actual) == set(expected)
+
+def test_group_sentences_into_pairs_bad_abstract():
+  records = [{
+        "sent_text": "first",
+        "sent_idx": 1,
+        "sent_total": 3,
+        "pmid": 123,
+        "version": 1,
+      }, {
+        "sent_text": "title",
+        "sent_idx": 0,
+        "sent_total": 3,
+        "pmid": 123,
+        "version": 1,
+      }, {
+        "sent_text": "ignore me!",
+        "sent_idx": 0,
+        "sent_total": 1,
+        "pmid": 987,
+        "version": 1,
+      }, {
+        "sent_text": "second",
+        "sent_idx": 2,
+        "sent_total": 3,
+        "pmid": 123,
+        "version": 1,
+  }]
+  actual = util.group_sentences_into_pairs(records)
+  expected = [
+      ("title", "first"),
+      ("first", "second"),
+  ]
+  assert set(actual) == set(expected)
+
+def test_group_sentences_into_pairs_separate_versions():
+  records = [{
+        "sent_text": "v1 first",
+        "sent_idx": 1,
+        "sent_total": 2,
+        "pmid": 123,
+        "version": 1,
+      }, {
+        "sent_text": "v1 title",
+        "sent_idx": 0,
+        "sent_total": 2,
+        "pmid": 123,
+        "version": 1,
+      }, {
+        "sent_text": "v2 title",
+        "sent_idx": 0,
+        "sent_total": 2,
+        "pmid": 123,
+        "version": 2,
+      }, {
+        "sent_text": "v2 first",
+        "sent_idx": 1,
+        "sent_total": 2,
+        "pmid": 123,
+        "version": 2,
+  }]
+  actual = util.group_sentences_into_pairs(records)
+  expected = [
+      ("v1 title", "v1 first"),
+      ("v2 title", "v2 first"),
+  ]
+  assert set(actual) == set(expected)
--- pymoliere/ml/abstract_generator/util.py
+++ pymoliere/ml/abstract_generator/util.py
@@ -1,6 +1,10 @@
-from typing import Dict, Any
+from typing import Dict, Any, Iterable
+from pymoliere.util.misc_util import Record
 from transformers import BertModel, BertTokenizer
 import torch
+from copy import copy
+from typing import List
+from random import random
 
 class AbstractGenerator(BertModel):
   def __init__(self, config:Dict[str, Any]):
@@ -30,3 +34,64 @@ class AbstractGenerator(BertModel):
     # reconstitute correct shape
     x = x.view(batch_size, seq_len, self.config.vocab_size)
     return x
+
+def group_sentences_into_pairs(records:Iterable[Record])->Iterable[Record]:
+  """
+  Expect records to have the following fields:
+        "sent_text": str,
+        "sent_idx": int,
+        "sent_total": int,
+        "pmid": int,
+        "version": int,
+  """
+  res = []
+  key2text = {}
+  for record in records:
+    key = (record["pmid"], record["version"], record["sent_idx"])
+    value = record["sent_text"]
+    # Don't want dups
+    assert key not in key2text
+    key2text[key] = value
+    prev_key = (record["pmid"], record["version"], record["sent_idx"]-1)
+    next_key = (record["pmid"], record["version"], record["sent_idx"]+1)
+    if prev_key in key2text:
+      res.append((key2text[prev_key], key2text[key]))
+    if next_key in key2text:
+      res.append((key2text[key], key2text[next_key]))
+  return res
+
+def mask_sequence(
+    tokenizer:BertTokenizer,
+    sequence:List[int],
+    mask:List[bool],
+)->List[int]:
+  """
+  If mask[i] is True, then we replace sequence[i] with tokenizer.mask_token_id.
+  Returns a COPY
+  """
+
+  sequence = copy(sequence)
+  assert len(sequence) == len(mask)
+  for idx in range(len(sequence)):
+    if mask[idx]:
+      sequence[idx] = tokenizer.mask_token_id
+  return sequence
+
+def generate_sentence_mask(
+    tokenizer:BertTokenizer,
+    sequence:List[int],
+    per_token_mask_prob:float,
+)->List[bool]:
+  assert per_token_mask_prob >= 0
+  assert per_token_mask_prob <= 1
+  assert sequence[0] == tokenizer.cls_token_id
+  assert sequence[-1] == tokenizer.sep_token_id
+  mid_sep_idx = sequence.find(tokenizer.sep_token_id)
+  # Must find a separation token between the start and end
+  assert mid_sep_idx < len(sequence)-1
+  # init mask to 0
+  mask = [False] * len(sequence)
+  for idx in range(mid_sep_idx+1, len(sequence)-1):
+    if random() <= per_token_mask_prob:
+      mask[idx] = True
+  return mask

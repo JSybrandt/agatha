94fa98ef9aa65b7159c518bc036b98d7c5eaed30
JSybrandt <nalta45@gmail.com>
2019-12-18 13:26:13 -0500

    enable dependency parsing in text processing

--- pymoliere/construct/text_util.py
+++ pymoliere/construct/text_util.py
@@ -89,7 +89,7 @@ def get_scispacy_initalizer(
     scispacy_version:Path
 )->Tuple[str, dpg.Initializer]:
   def _init():
-    return spacy.load(scispacy_version, disable=["parser"])
+    return spacy.load(scispacy_version)
   return "text_util:nlp", _init
 
 def get_stopwordlist_initializer(
@@ -353,7 +353,7 @@ def analyze_sentences(
           "lemma": tok.lemma_,
           "pos": tok.pos_,
           "tag": tok.tag_,
-          #"dep": tok.dep_,
+          "dep": tok.dep_,
           "stop": \
               tok.lemma_ in stopwords or tok.text.strip().lower() in stopwords
         }

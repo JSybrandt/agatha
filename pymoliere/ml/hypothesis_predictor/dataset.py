0fe8a0c72059b3e0ac593b0f8ebfcc6eae7e66eb
JSybrandt <nalta45@gmail.com>
2020-01-21 22:25:14 -0500

    Update hypothesis predictor with new code

--- pymoliere/ml/hypothesis_predictor/dataset.py
+++ pymoliere/ml/hypothesis_predictor/dataset.py
@@ -9,6 +9,25 @@ from typing import Dict, Tuple, Any, List
 import random
 from copy import deepcopy
 from itertools import chain
+from pymoliere.util.sqlite3_graph import Sqlite3Graph
+from dataclasses import dataclass
+import numpy as np
+
+@dataclass
+class HypothesisBatch:
+  subject_embedding:torch.FloatTensor
+  object_embedding:torch.FloatTensor
+  subject_neighbor_embeddings:torch.FloatTensor
+  object_neighbor_embeddings:torch.FloatTensor
+  label:torch.FloatTensor
+
+@dataclass
+class PredicateObservation:
+  subject_embedding:np.array
+  object_embedding:np.array
+  subject_neighbor_embeddings:List[np.array]
+  object_neighbor_embeddings:List[np.array]
+  label:int
 
 IDX2VERB = [
     "administered_to", "affects", "associated_with", "augments", "causes",
@@ -32,15 +51,15 @@ VERB2IDX = {v:i for i, v in enumerate(IDX2VERB)}
 class PredicateLoader(torch.utils.data.Dataset):
   def __init__(
       self,
+      embedding_index:EmbeddingIndex,
+      graph_index:Sqlite3Graph,
       entity_dir:Path,
-      embedding_dir:Path,
+      neighbors_per_term:int,
   ):
-    self.entity_index = EntityIndex(entity_dir, entity_type=dbu.PREDICATE_TYPE)
-    self.embedding_index = PreloadedEmbeddingIndex(
-        embedding_dir=embedding_dir,
-        entity_dir=entity_dir,
-        select_entity_type=dbu.MESH_TERM_TYPE,
-    )
+    self.predicate_index = EntityIndex(entity_dir, entity_type=dbu.PREDICATE_TYPE)
+    self.embedding_index = embedding_index
+    self.graph_index = graph_index
+    self.neighbors_per_term = neighbors_per_term
 
   @staticmethod
   def parse_predicate_name(predicate_name:str)->Tuple[str, str, str]:
@@ -50,110 +69,149 @@ class PredicateLoader(torch.utils.data.Dataset):
     return components[1:]
 
   def __len__(self):
-    return len(self.entity_index)
+    return len(self.predicate_index)
 
-  def __getitem__(self, idx:int)->Dict[str, Any]:
-    subj, verb, obj = self.parse_predicate_name(self.entity_index[idx])
+  def _sample_relevant_neighbors(
+      self,
+      term:str,
+      exclude:str,
+  )->List[str]:
+    items = [n for n in self.graph_index[term] if n is not exclude]
+    if len(items) <= self.neighbors_per_term:
+      return items
+    else:
+      return random.sample(items, self.neighbors_per_term)
+
+  def __getitem__(self, idx:int)->PredicateObservation:
+    predicate = self.predicate_index[idx]
+    subj, _, obj = self.parse_predicate_name(predicate)
     subj = f"{dbu.MESH_TERM_TYPE}:{subj}"
     obj = f"{dbu.MESH_TERM_TYPE}:{obj}"
-    return dict(
-        subj_name=subj,
-        subj_emb=self.embedding_index[subj],
-        obj_name=obj,
-        obj_emb=self.embedding_index[obj],
-        verb_name=verb,
+    subj_neigh = self._sample_relevant_neighbors(subj, predicate)
+    obj_neigh = self._sample_relevant_neighbors(obj, predicate)
+    return PredicateObservation(
+        subject_embedding=self.embedding_index[subj],
+        object_embedding=self.embedding_index[obj],
+        subject_neighbor_embeddings=[
+          self.embedding_index[n] for n in subj_neigh
+        ],
+        object_neighbor_embeddings=[
+          self.embedding_index[n] for n in obj_neigh
+        ],
+        label=1
     )
+
+
+def predicate_collate(
+    positive_samples:List[PredicateObservation],
+    num_negative_samples:int,
+    neighbors_per_term:int,
+)->Dict[str, Any]:
+
+  negative_samples:List[PredicateObservation] = []
+  if num_negative_samples > 0:
+    all_neighbors = list(chain.from_iterable(map(
+        lambda x: chain(
+          x.subject_neighbor_embeddings,
+          x.object_neighbor_embeddings,
+        ),
+        positive_samples
+    )))
+    all_entities = list(chain.from_iterable(map(
+        lambda x: [x.subject_embedding, x.object_embedding],
+        positive_samples
+    )))
+    for _ in range(num_negative_samples):
+      negative_samples.append(PredicateObservation(
+        subject_embedding=random.choice(all_entities),
+        object_embedding=random.choice(all_entities),
+        subject_neighbor_embeddings=[
+          random.choice(all_neighbors)
+          for _ in range(random.randint(1, num_negative_samples))
+        ],
+        object_neighbor_embeddings=[
+          random.choice(all_neighbors)
+          for _ in range(random.randint(1, num_negative_samples))
+        ],
+        label=0,
+      ))
+
+  samples = positive_samples + negative_samples
+  random.shuffle(samples)
+
+  return HypothesisBatch(
+      subject_embedding=torch.FloatTensor([
+        s.subject_embedding for s in samples
+      ]),
+      object_embedding=torch.FloatTensor([
+        s.object_embedding for s in samples
+      ]),
+      subject_neighbor_embeddings=torch.nn.utils.rnn.pad_sequence([
+        torch.FloatTensor(s.subject_neighbor_embeddings)
+        for s in samples
+      ]),
+      object_neighbor_embeddings=torch.nn.utils.rnn.pad_sequence([
+        torch.FloatTensor(s.object_neighbor_embeddings)
+        for s in samples
+      ]),
+      label=torch.FloatTensor([
+        s.label for s in samples
+      ]),
+  )
+
 
 class TestPredicateLoader(torch.utils.data.Dataset):
   def __init__(
       self,
       test_data_dir:Path,
-      embedding_index:EmbeddingIndex
+      embedding_index:EmbeddingIndex,
+      graph_index:Sqlite3Graph,
+
   ):
-    published_path = test_data_dir.joinpath("published.txt")
-    noise_path = test_data_dir.joinpath("noise.txt")
+    self.embedding_index = embedding_index
+    self.graph_index = graph_index
+    published_path = Path(test_data_dir).joinpath("published.txt")
+    noise_path = Path(test_data_dir).joinpath("noise.txt")
     assert published_path.is_file()
     assert noise_path.is_file()
-    self.predicates = []
+    self.subjs_objs_labels = []
     num_failures = 0
     for path in [published_path, noise_path]:
       with open(path) as pred_file:
         for line in pred_file:
           subj, obj, year = line.lower().strip().split("|")
-          subj = f"{dbu.MESH_TERM_TYPE}:{subj}"
-          obj = f"{dbu.MESH_TERM_TYPE}:{obj}"
-          if subj in embedding_index and obj in embedding_index:
-            self.predicates.append(dict(
-              subj_name=subj,
-              subj_emb=embedding_index[subj],
-              obj_name=obj,
-              obj_emb=embedding_index[obj],
-              verb_name="UNKNOWN" if path == published_path else "INVALID",
-            ))
-          else:
-            print("FAILED:", line.strip())
-            num_failures += 1
-    if num_failures > 0:
-      print("Failed:", num_failures)
-      print("Succeeded:", len(self.predicates))
+          if subj in graph_index and obj in graph_index:
+            label = 1 if int(year) > 0 else 0
+            self.subjs_objs_labels[(subj, obj, label)]
+
+  def _sample_relevant_neighbors(
+      self,
+      term:str,
+      exclude:str,
+  )->List[str]:
+    items = [n for n in self.graph_index[term] if n is not exclude]
+    if len(items) <= self.neighbors_per_term:
+      return items
+    else:
+      return random.sample(items, self.neighbors_per_term)
 
   def __len__(self):
-    return len(self.predicates)
+    return len(self.subjs_objs_labels)
 
-  def __getitem__(self, idx):
-    return self.predicates[idx]
-
-def predicate_collate(
-    predicate_data:List[Dict[str, Any]],
-    num_negative_samples:int,
-    name2idx:Dict[str, Any],
-)->Dict[str, Any]:
-  def gen_neg_sample():
-    subj = random.choice(predicate_data)
-    obj = random.choice(predicate_data)
-    while obj == subj:
-      obj = random.choice(predicate_data)
-    return dict(
-        subj_name=subj["subj_name"],
-        subj_emb=subj["subj_emb"],
-        obj_name=subj["obj_name"],
-        obj_emb=subj["obj_emb"],
-        verb_name="INVALID"
+  def __getitem__(self, idx:int)->PredicateObservation:
+    subj, obj, label = self.subjs_objs_labels[idx]
+    subj = f"{dbu.MESH_TERM_TYPE}:{subj}"
+    obj = f"{dbu.MESH_TERM_TYPE}:{obj}"
+    subj_neigh = self._sample_relevant_neighbors(subj, predicate)
+    obj_neigh = self._sample_relevant_neighbors(obj, predicate)
+    return PredicateObservation(
+        subject_embedding=self.embedding_index[subj],
+        object_embedding=self.embedding_index[obj],
+        subject_neighbor_embeddings=[
+          self.embedding_index[n] for n in subj_neigh
+        ],
+        object_neighbor_embeddings=[
+          self.embedding_index[n] for n in obj_neigh
+        ],
+        label=label,
     )
-  def safe_get(name, name2idx):
-    name_or_none = name2idx.get(name)
-    if name_or_none is None:
-      return name2idx["UNKNOWN"]
-    return name_or_none
-
-  subj_emb = []
-  obj_emb = []
-  verb_idx = []
-  subj_idx = []
-  obj_idx = []
-  labels = []
-  neg_samples = [gen_neg_sample() for _ in range(num_negative_samples)]
-  for pred in chain(predicate_data, neg_samples):
-    subj_idx.append(safe_get(pred["subj_name"], name2idx))
-    obj_idx.append(safe_get(pred["obj_name"], name2idx))
-    subj_emb.append(pred["subj_emb"])
-    obj_emb.append(pred["obj_emb"])
-    safe_verb_idx = safe_get(pred["verb_name"], VERB2IDX)
-    verb_idx.append(safe_verb_idx)
-    label = 0 if safe_verb_idx == VERB2IDX["INVALID"] else 1
-    labels.append(label)
-
-  assert len(subj_emb) == len(obj_emb) == len(verb_idx) == len(labels)
-  return dict(
-      # batch
-      subj_idx=torch.LongTensor(subj_idx),
-      # batch X emb_dim
-      subj_emb=torch.FloatTensor(subj_emb),
-      # batch
-      obj_idx=torch.LongTensor(obj_idx),
-      # batch X emb_dim
-      obj_emb=torch.FloatTensor(obj_emb),
-      # batch
-      verbs=torch.LongTensor(verb_idx),
-      labels=torch.FloatTensor(labels),
-  )
--- pymoliere/ml/hypothesis_predictor/hypothesis_predictor.py
+++ pymoliere/ml/hypothesis_predictor/hypothesis_predictor.py
@@ -6,6 +6,7 @@ from pymoliere.ml.hypothesis_predictor.dataset import (
     PredicateLoader,
     predicate_collate,
     TestPredicateLoader,
+    HypothesisBatch,
 )
 from typing import Dict, Any
 from pathlib import Path
@@ -14,243 +15,146 @@ from pymoliere.ml.util.embedding_index import EmbeddingIndex
 from pymoliere.ml.util.entity_index import EntityIndex
 import pymoliere.util.database_util as dbu
 from copy import deepcopy
+from pymoliere.util.sqlite3_graph import Sqlite3Graph
 
 class HypothesisPredictor(pl.LightningModule):
   def __init__(self, hparams:Namespace):
     super(HypothesisPredictor, self).__init__()
     self.hparams = hparams
-    # self.hparams.sqlite_embedding_location = Path(self.hparams.sqlite_embedding_location)
-    # self.hparams.embedding_dir = Path(self.hparams.embedding_dir)
-    # self.hparams.entity_dir = Path(self.hparams.entity_dir)
-    # self.hparams.test_data_dir = Path(self.hparams.test_data_dir)
-
-    self.name2idx = {}
-    entity_index = EntityIndex(self.hparams.entity_dir, dbu.MESH_TERM_TYPE)
-    for idx in range(len(entity_index)):
-      self.name2idx[entity_index[idx]] = idx
-    self.name2idx["UNKNOWN"] = len(self.name2idx)
-
-    self.entity_embedding = torch.nn.Embedding(
-        len(self.name2idx),
-        self.hparams.dim,
-    )
-    #self.merger = torch.nn.Linear(self.hparams.dim+16, self.hparams.dim)
-
-    self.h1 = torch.nn.Linear(self.hparams.dim*2, self.hparams.dim*2)
-    self.h2 = torch.nn.Linear(self.hparams.dim*2, self.hparams.dim)
-    self.h3 = torch.nn.Linear(self.hparams.dim, int(self.hparams.dim/2))
-    self.predict_label = torch.nn.Linear(int(self.hparams.dim/2), 1)
-    self.predict_verb = torch.nn.Linear(int(self.hparams.dim/2), len(VERB2IDX))
-
-    self.label_loss_fn = torch.nn.BCELoss()
-    self.verb_loss_fn = torch.nn.NLLLoss()
-    # TRAIN MODE
-    if self.hparams.mode == "train":
-      predicate_dataset = PredicateLoader(
-        entity_dir=self.hparams.entity_dir,
+    # Helper data structures
+    self.embedding_index = EmbeddingIndex(
         embedding_dir=self.hparams.embedding_dir,
-      )
-      val_size = int(len(predicate_dataset)*self.hparams.validation_fraction)
-      train_size = len(predicate_dataset) - val_size
-      self.train_predicates, self.val_predicates = \
-          torch.utils.data.random_split(
-            predicate_dataset, [train_size, val_size]
-          )
-    else:
-      self.train_predicates = []
-      self.val_predicates = []
-    # TEST MODE
-    if self.hparams.mode == "test":
-      with EmbeddingIndex(
-            embedding_dir=self.hparams.embedding_dir,
-            emb_loc_db_path=self.hparams.sqlite_embedding_location,
-      ) as embedding_index:
-        self.test_predicates = TestPredicateLoader(
-          self.hparams.test_data_dir,
-          embedding_index,
-        )
-    else:
-      self.test_predicates = []
+        emb_loc_db_path=self.hparams.sqlite_embedding_path,
+    ).__enter__()
+    self.graph_index = Sqlite3Graph(self.hparams.sqlite_graph_path).__enter__()
 
-  def forward(self, model_in:Dict[str, Any])->torch.FloatTensor:
-    device = next(self.parameters()).device
-    #subjects = model_in["subj_emb"].to(device)
-    #objects = model_in["obj_emb"].to(device)
-    subj_indices = model_in["subj_idx"].to(device)
-    obj_indices = model_in["obj_idx"].to(device)
-
-    # subjects = torch.cat((subjects, self.entity_embedding(subj_indices)), dim=1)
-    # subjects = self.merger(subjects)
-    # objects = torch.cat((objects, self.entity_embedding(obj_indices)), dim=1)
-    # objects = self.merger(objects)
-    subjects = self.entity_embedding(subj_indices)
-    objects = self.entity_embedding(obj_indices)
-    joint = torch.cat((subjects, objects), dim=1)
-    joint = torch.relu(self.h1(joint))
-    joint = torch.relu(self.h2(joint))
-    joint = torch.relu(self.h3(joint))
-
-    #predicted_labels = self.predict_label(joint)
-    #predicted_labels = torch.sigmoid(predicted_labels).reshape(-1)
-    predicted_verbs = self.predict_verb(joint)
-    predicted_verbs = torch.log_softmax(predicted_verbs, dim=1)
-    return dict(
-        #labels=predicted_labels.reshape(-1),
-        verbs=predicted_verbs,
+    # All predicates, will split
+    predicates = PredicateLoader(
+      embedding_index=self.embedding_index,
+      graph_index=self.graph_index,
+      entity_dir=self.hparams.entity_dir,
+      neighbors_per_term=self.hparams.neighbors_per_term,
     )
+    # Validation samples
+    val_size = int(len(predicates)*self.hparams.validation_fraction)
+    train_size = len(predicates) - val_size
+    # split the dataset in two uneven parts
+    self.training_data, self.val_data = torch.utils.data.random_split(
+        predicates, [train_size, val_size]
+    )
+    # configure test
+    self.test_data = TestPredicateLoader(
+        self.hparams.test_data_dir,
+        self.embedding_index,
+        self.graph_index,
+    )
+    # Layers
+    # This is going to transform into {-1, 1}
+    self.embedding_transformation = torch.nn.Linear(
+        self.hparams.dim, self.hparams.dim
+    )
+    # Extra
+    self.loss_fn = torch.nn.BCELoss()
+
+  def _batch_to_device(self, b:HypothesisBatch)->HypothesisBatch:
+    device = next(self.parameters()).device
+    return HypothesisBatch(
+        subject_embedding=b.subject_embedding.to(device),
+        object_embedding=b.object_embedding.to(device),
+        subject_neighbor_embeddings=b.subject_neighbor_embeddings.to(device),
+        object_neighbor_embeddings=b.object_neighbor_embeddings.to(device),
+        label=b.label.to(device),
+    )
+
+  def forward(self, batch:HypothesisBatch)->torch.FloatTensor:
+    # batch X dim
+    local_subj_emb = self.embedding_transformation(batch.subject_embedding)
+    local_subj_emb = torch.tanh(local_subj_emb)
+    # batch X dim
+    local_obj_emb = self.embedding_transformation(batch.object_embedding)
+    local_obj_emb = torch.tanh(local_obj_emb)
+    # # neigh_seq X batch X dim
+    # local_subj_neig_embs = self.embedding_transformation(
+        # batch.subject_neighbor_embeddings
+    # )
+    # local_subj_neig_embs = torch.tanh(local_subj_neig_embs)
+    # # neigh_seq X batch X dim
+    # local_obj_neig_embs = self.embedding_transformation(
+        # batch.object_neighbor_embeddings
+    # )
+    # local_obj_neig_embs = torch.tanh(local_obj_neig_embs)
+    # |batch|
+    subj_obj_dots = (local_subj_emb * local_obj_emb).sum(dim=1)
+    return torch.sigmoid(subj_obj_dots)
+
 
   def training_step(self, batch, batch_idx):
+    batch = self._batch_to_device(batch)
     predictions = self.forward(batch)
     device = next(self.parameters()).device
-    #true_labels = batch["labels"].to(device)
-    true_verbs = batch["verbs"].to(device)
-    #label_loss = self.label_loss_fn(predictions["labels"], true_labels)
-    verb_loss = self.verb_loss_fn(predictions["verbs"], true_verbs)
-    loss = verb_loss
-    #loss = label_loss + verb_loss
-    #label_accuracy = (
-    #    ((predictions["labels"] > 0.5) == (true_labels > 0.5)).sum()
-    #    / float(len(predictions["labels"]))
-    #)
-    verb_accuracy = (
-        (predictions["verbs"].argmax(dim=1) == true_verbs).sum()
-        / float(len(predictions["verbs"]))
-    )
     metrics=dict(
-        loss=loss,
-        #label_loss=label_loss,
-        #verb_loss=verb_loss,
-        #label_accuracy=label_accuracy,
-        verb_accuracy=verb_accuracy,
+        loss=self.loss_fn(predictions, batch.label),
+        accuracy=(
+          ((predictions > 0.5) == (batch.label > 0.5))
+          .sum().float() / float(len(predictions))
+        ),
     )
     return {
-        'loss': loss,
+        'loss': metrics["loss"],
         'progress_bar': metrics,
         'log': metrics,
     }
 
   def validation_step(self, batch, batch_idx):
-    predictions = self.forward(batch)
-    device = next(self.parameters()).device
-    #true_labels = batch["labels"].to(device)
-    true_verbs = batch["verbs"].to(device)
-    #label_loss = self.label_loss_fn(predictions["labels"], true_labels)
-    verb_loss = self.verb_loss_fn(predictions["verbs"], true_verbs)
-    #loss = label_loss + verb_loss
-    # label_accuracy = (
-        # ((predictions["labels"] > 0.5) == (true_labels > 0.5)).sum()
-        # / float(len(predictions["labels"]))
-    # )
-    verb_accuracy = (
-        (predictions["verbs"].argmax(dim=1) == true_verbs).sum()
-        / float(len(predictions["verbs"]))
-    )
-    return dict(
-        loss=verb_loss,
-        #loss=loss,
-        #label_loss=label_loss,
-        #verb_loss=verb_loss,
-        #label_accuracy=label_accuracy,
-        verb_accuracy=verb_accuracy,
-    )
-
-  def validation_end(self, outputs):
-    mean_loss = torch.stack([x["loss"] for x in outputs]).mean()
-    #mean_label_accruacy = torch.stack([x["label_accuracy"] for x in outputs]).mean()
-    #mean_verb_accruacy = torch.stack([x["verb_accuracy"] for x in outputs]).mean()
-    vals = dict(
-        val_loss=mean_loss,
-        #label_accruacy=mean_label_accruacy,
-        #verb_accruacy=mean_verb_accruacy,
-    )
-    print(vals)
-    return vals
-
+    return self.training_step(batch, batch_idx)["log"]
 
   def test_step(self, batch, batch_idx):
-    predictions = self.forward(batch)
-    device = next(self.parameters()).device
-    true_labels = batch["labels"].to(device)
-    true_verbs = batch["verbs"].to(device)
-    label_loss = self.label_loss_fn(predictions["labels"], true_labels)
-    verb_loss = self.verb_loss_fn(predictions["verbs"], true_verbs)
-    loss = label_loss + verb_loss
-    label_accuracy = (
-        ((predictions["labels"] > 0.5) == (true_labels > 0.5)).sum()
-        / float(len(predictions["labels"]))
-    )
-    verb_accuracy = (
-        (predictions["verbs"].argmax(dim=1) == true_verbs).sum()
-        / float(len(predictions["verbs"]))
-    )
-    return dict(
-        loss=loss,
-        label_loss=label_loss,
-        verb_loss=verb_loss,
-        label_accuracy=label_accuracy,
-        verb_accuracy=verb_accuracy,
-    )
+    return self.training_step(batch, batch_idx)["log"]
+
+  def _on_end(self, outputs):
+    metrics = {}
+    for metric in outputs[0]:
+      metrics[metric] = torch.stack([x[metric] for x in outputs]).mean()
+    return metrics
+
+  def validation_end(self, outputs):
+    return self._on_end(outputs)
 
   def test_end(self, outputs):
-    mean_loss = torch.stack([x["loss"] for x in outputs]).mean()
-    mean_label_accruacy = torch.stack([x["label_accuracy"] for x in outputs]).mean()
-    mean_verb_accruacy = torch.stack([x["verb_accuracy"] for x in outputs]).mean()
-    vals = dict(
-        test_loss=mean_loss,
-        label_accruacy=mean_label_accruacy,
-        verb_accruacy=mean_verb_accruacy,
+    return self._on_end(outputs)
+
+  def _config_dl(self, dataset):
+    shuffle=True
+    sampler=None
+    if self.hparams.distributed:
+      shuffle=False
+      sampler=torch.utils.data.distributed.DistributedSampler(dataset)
+    collate = lambda batch: predicate_collate(
+        positive_samples=batch,
+        num_negative_samples=int(
+          len(batch) * self.hparams.negative_sample_ratio
+        ),
+        neighbors_per_term=self.hparams.neighbors_per_term,
     )
-    print(vals)
-    return vals
+    return torch.utils.data.DataLoader(
+        dataset=self.training_data,
+        shuffle=shuffle,
+        sampler=sampler,
+        batch_size=self.hparams.batch_size,
+        collate_fn=collate,
+    )
 
   @pl.data_loader
   def train_dataloader(self):
-    if self.hparams.distributed:
-      shuffle=False
-      sampler=torch.utils.data.distributed.DistributedSampler(self.train_predicates)
-    else:
-      sampler = None
-      shuffle=True
-    if len(self.train_predicates) == 0:
-      shuffle = False
-    collate = lambda x: predicate_collate(x, len(x), self.name2idx)
-    return torch.utils.data.DataLoader(
-        dataset=self.train_predicates,
-        shuffle=shuffle,
-        sampler=sampler,
-        batch_size=int(self.hparams.batch_size/2),
-        # Collate will double the batch
-        collate_fn=collate,
-        pin_memory=True,
-    )
+    return self._config_dl(self.training_data)
 
   @pl.data_loader
   def val_dataloader(self):
-    if self.hparams.distributed:
-      sampler=torch.utils.data.distributed.DistributedSampler(self.val_predicates)
-    else:
-      sampler = None
-    collate = lambda x: predicate_collate(x, len(x), self.name2idx)
-    return torch.utils.data.DataLoader(
-        dataset=self.val_predicates,
-        sampler=sampler,
-        batch_size=int(self.hparams.batch_size/2),
-        # Collate will double the batch
-        collate_fn=collate,
-        pin_memory=True,
-    )
+    return self._config_dl(self.val_data)
 
   @pl.data_loader
   def test_dataloader(self):
-    collate = lambda x: predicate_collate(x, 0, self.name2idx)
-    return torch.utils.data.DataLoader(
-        dataset=self.test_predicates,
-        #sampler=sampler,
-        batch_size=self.hparams.batch_size,
-        # Collate will double the batch
-        collate_fn=collate,
-        pin_memory=True,
-    )
+    return self._config_dl(self.test_data)
 
   def configure_optimizers(self):
     return Lamb(
@@ -259,6 +163,7 @@ class HypothesisPredictor(pl.LightningModule):
         lr=self.hparams.lr,
         weight_decay=0.01,
     )
+
   def optimizer_step(
       self,
       epoch_idx,
@@ -281,7 +186,11 @@ class HypothesisPredictor(pl.LightningModule):
   @staticmethod
   def configure_argument_parser(parser:ArgumentParser)->ArgumentParser:
     parser.add_argument(
-        "--sqlite-embedding-location",
+        "--sqlite-embedding-path",
+        help="Location of the db containing references for node's embeddings."
+    )
+    parser.add_argument(
+        "--sqlite-graph-path",
         help="Location of the db containing references for node's embeddings."
     )
     parser.add_argument(
@@ -302,6 +211,11 @@ class HypothesisPredictor(pl.LightningModule):
         default=512,
     )
     parser.add_argument(
+        "--negative-sample-ratio",
+        type=float,
+        default=5,
+    )
+    parser.add_argument(
         "--lr",
         type=float,
         default=0.02,
@@ -320,6 +234,11 @@ class HypothesisPredictor(pl.LightningModule):
         "--validation-fraction",
         type=float,
         default=0.1,
+    )
+    parser.add_argument(
+        "--neighbors-per-term",
+        type=int,
+        default=5,
     )
     parser.add_argument(
         "--distributed",

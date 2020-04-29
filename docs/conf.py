690415b944803b05c0452caefc74ce7e71c5983e
JSybrandt <nalta45@gmail.com>
2020-04-29 11:21:19 -0400

    adds markdown to sphynx

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../agatha'))
 
 
 # -- Project information -----------------------------------------------------
@@ -32,6 +32,7 @@ release = '2020-04-29'
 # ones.
 extensions = [
     "recommonmark",  # Allow markdown
+    "sphinx.ext.autodoc",  # Make documentation from 
 ]
 
 # Add any paths that contain templates here, relative to this directory.

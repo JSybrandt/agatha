a6024dfaded3ca420c34eaf613b23758a37b1c13
JSybrandt <nalta45@gmail.com>
2020-05-18 10:51:38 -0400

    Updates SemMedDB conversion tool for updated graph data

--- tools/py_scripts/semmeddb_csv_to_edgelist.py
+++ tools/py_scripts/semmeddb_csv_to_edgelist.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from agatha.util import semmeddb_util as sm
 from agatha.util import entity_types as typs
 from typing import Iterable
+import json
 
 def get_sentence_key(predicate:sm.Predicate)->str:
   # version 1
@@ -36,25 +37,27 @@ def get_adjacent_names(predicate:sm.Predicate)->Iterable[str]:
 
 def main(
   semmeddb_csv_path:Path,
-  output_tsv_path:Path,
+  output_json_path:Path,
   cut_date:sm.Datestr=None,
   edge_weight:float=1.0,
 ):
   semmeddb_csv_path = Path(semmeddb_csv_path)
-  output_tsv_path = Path(output_tsv_path)
+  output_json_path = Path(output_json_path)
   assert semmeddb_csv_path.is_file()
-  assert not output_tsv_path.exists()
+  assert not output_json_path.exists()
 
   predicates = sm.parse(semmeddb_csv_path)
   if cut_date is not None:
     predicates = sm.filter_by_date(predicates, cut_date)
 
-  with open(output_tsv_path, 'w') as tsv_file:
+  with open(output_json_path, 'w') as json_file:
     for predicate in predicates:
       pred_key = sm.predicate_to_key(predicate)
       for neigh in get_adjacent_names(predicate):
-        tsv_file.write(f"{pred_key}\t{neigh}\t{edge_weight}\n")
-        tsv_file.write(f"{neigh}\t{pred_key}\t{edge_weight}\n")
+        json_file.write(json.dumps({"key": pred_key, "value": neigh}))
+        json_file.write("\n")
+        json_file.write(json.dumps({"key": neigh, "value": pred_key}))
+        json_file.write("\n")
 
 
 
